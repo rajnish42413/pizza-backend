53aedd7f3f2a2ecb9584087ce5c56565a9bfb853
rajnish42413 <rajnish42413@gmail.com>
2019-10-17 17:52:32 +0530

    pizza project setup

---
+++ app/Pizza.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+use App\PizzaImage;
+use App\PizzaTopping;
+use Illuminate\Database\Eloquent\Model;
+
+class Pizza extends Model
+{
+    protected $table = 'pizzas';
+
+    public function image()
+    {
+        return $this->hasOne(PizzaImage::class);
+    }
+    public function media()
+    {
+        return $this->hasMany(PizzaImage::class);
+    }
+
+    public function topping()
+    {
+        return $this->hasMany(PizzaImage::class);
+    }
+
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pizza Project | Backend</title>
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+     <div class="container-fluid mt-5 mb-5 bg-white">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="list-group">
+                  <a href="/" class="list-group-item active">API LIST</a>
+                  <a href="/pizza" target="_blank" class="list-group-item">Pizza List</a>
+                  <a href="#" class="list-group-item">Morbi leo risus</a>
+                  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                  <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </div>
+            </div>
+        </div>
+     </div>
+    </body>
+</html>
